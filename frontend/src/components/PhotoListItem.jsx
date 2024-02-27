4aad2429293f60355d70cfa2c542db4918f7e8b1
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-02-27 10:39:23 -0500

    create PhotoListItem component and pass props from App.jsx

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,19 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
-  /* Insert React */
+
+const PhotoListItem = (props) => {
+  const imageData = props.imageData;
+
+  return(
+    <div key={imageData.id}>
+      <img src={imageData.imageSource} />
+      <img src={imageData.profile} />
+      <p>{imageData.username}</p>
+      <p>{imageData.location.city}, {imageData.location.country}</p>
+    </div>
+  )
 };
 
 export default PhotoListItem;
