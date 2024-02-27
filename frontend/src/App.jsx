9695bd421082f415df8666ab759cbb55c4168cd1
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-02-27 12:31:59 -0500

    clean up commented out code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,19 +6,9 @@ import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  // const photos = new Array(3);
-  
-  // for (let i = 0; i < photos.length; i++){
-  //   photos[i] = {...sampleDataForPhotoListItem};
-  //   photos[i].id = i + 1;
-  // }
+
   return (
     <div className="App">
-      {/* {
-        photos.map(photo => 
-          <PhotoListItem key={photo.id} imageData={photo}/>
-        )
-      } */}
       <PhotoList />
     </div>
   );
