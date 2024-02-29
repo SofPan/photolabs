2ec33a8f95045001ab28f063c031e864acacd0b5
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-02-29 12:29:04 -0500

    edit top nav bar styles per design feedback

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -8,8 +8,10 @@ const TopNavigation = ({topics, hasFavourite}) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <div className="top-nav-bar__topics">
       <TopicList topics={topics}/>
       <FavBadge selected={hasFavourite} isFavPhotoExist={hasFavourite}/>
+      </div>
     </div>
   )
 }
